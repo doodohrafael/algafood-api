932077e014efc237fb9b3dadaf6f1aa500dedb72
Rafael <rafael_profissional@outlook.com>
2022-01-31 11:47:09 -0300

    serviços rest GET, POST E PUT de estado implementado. São dois serviços GETs, um traz uma lista com todos os estados, e o outro somente um singleton resource

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -3,12 +3,21 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
+import com.algaworks.algafood.domain.service.CadastroEstadoService;
 
 @RestController
 @RequestMapping("/estados")
@@ -16,10 +25,39 @@ public class EstadoController {
 
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CadastroEstadoService cadastroEstado;
 
 	@GetMapping
 	public List<Estado> listar() {
 		return estadoRepository.listar();
 	}
+	
+	@GetMapping("/{estadoId}")
+	public ResponseEntity<Estado> buscar(@PathVariable Long estadoId) {
+		Estado estado = cadastroEstado.buscar(estadoId);
+		if(estado != null) {
+			return ResponseEntity.ok(estado);
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estado> adicionar(@RequestBody Estado estado) {
+		estado = cadastroEstado.salvar(estado);
+		return ResponseEntity.status(HttpStatus.CREATED).body(estado);
+	}
+	
+	@PutMapping("/{estadoId}")
+	public ResponseEntity<Estado> atualizar(@PathVariable Long estadoId, @RequestBody Estado estado) {
+		try {
+			estado = cadastroEstado.atualizar(estadoId, estado);
+			return ResponseEntity.status(HttpStatus.OK).body(estado);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
 	
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+
+@Service
+public class CadastroEstadoService {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	public Estado buscar(Long estadoId) {
+		return estadoRepository.porId(estadoId);
+	}
+
+	public Estado salvar(Estado estado) {
+		estado = estadoRepository.adicionar(estado);
+		return estado;
+	}
+
+	public Estado atualizar(Long estadoId, Estado estado) {
+		try {
+			Estado estadoAtual = estadoRepository.porId(estadoId);
+			BeanUtils.copyProperties(estado, estadoAtual, "id");
+			estadoRepository.adicionar(estadoAtual);
+			return estadoAtual;
+		} catch (IllegalArgumentException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe um cadastro de estado com código %d", estadoId));
+		}
+	}
+}
