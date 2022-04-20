e698bc343494146b0be426dad686fb1cc526a86f
Rafael <rafael_profissional@outlook.com>
2022-04-20 13:49:35 -0300

    messages.properties added at project

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +32,13 @@ import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	
+	@Autowired
+	private MessageSource messageSource;
+	
+	public static final String MSG_ERRO_GENERICA_USUARIO_FINAL = 
+			"Ocorreu um erro interno inesperado no sistema. Tente novamente e se o "
+					+ "problema persistir, entre em contato com o administrador do sistema.";
+	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -36,10 +46,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente. ";
 		BindingResult bindingResult = ex.getBindingResult();
 		List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-				.map(fieldError -> Problem.Field.builder()
+				.map(fieldError -> {
+					String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+					return Problem.Field.builder()
 						.name(fieldError.getField())
-						.userMessage(fieldError.getDefaultMessage())
-						.build())
+						.userMessage(message)
+						.build();
+				})
 				.collect(Collectors.toList());
 		Problem problem = createProblemBuilder(status, problemType, detail)
 				.fields(problemFields)
@@ -48,10 +61,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
-	
-	public static final String MSG_ERRO_GENERICA_USUARIO_FINAL = 
-			"Ocorreu um erro interno inesperado no sistema. Tente novamente e se o "
-					+ "problema persistir, entre em contato com o administrador do sistema.";
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
